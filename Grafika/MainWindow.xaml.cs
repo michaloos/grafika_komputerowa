8f827d489796b8999874fe0098553e16386a9b55
michaloos <misialek1998@gmail.com>
2021-11-06 19:05:09 +0100

    podstawowa binaryzacja, wczytywanie pliku, grayscale

--- Grafika/MainWindow.xaml.cs
+++ Grafika/MainWindow.xaml.cs
@@ -2005,26 +2005,148 @@ namespace Grafika
             ScaleTransform scale = new ScaleTransform(zoom, zoom);
             ps4Image.LayoutTransform = scale;
         }
+        #endregion
 
         private void binByUserValue_Click(object sender, RoutedEventArgs e)
         {
+            if (ps5Image.Source == null || grayScaleValuePS5 == false || binUserValue.Text.Equals(""))
+            {
+                return;
+            }
+            indicatorPS5.IsBusy = true;
+            int binValue = int.Parse(binUserValue.Text);
+            Bitmap bitmap = ImageSourceToBitmap(ps5Image.Source);
+            BitmapImage newBitmap = null;
+            Bitmap tempBM = bitmap;
+            var task = Task.Run(() =>
+            {
+                if (binValue < 0 || binValue > 255)
+                {
+                    MessageBoxResult result = MessageBox.Show("Wartość powinna mieścić się od 0 do 255!");
+                }
+                else
+                {
+                    for (int i = 0; i < tempBM.Width; i++)
+                    {
+                        for (int j = 0; j < tempBM.Height; j++)
+                        {
+                            if (tempBM.GetPixel(i, j).R >= binValue)
+                            {
+                                tempBM.SetPixel(i, j, System.Drawing.Color.White);
+                            }
+                            else
+                            {
+                                tempBM.SetPixel(i, j, System.Drawing.Color.Black);
+                            }
+                        }
+                    }
+                }
+            });
+            
 
+            task.ContinueWith((t) =>
+            {
+                Application.Current.Dispatcher.Invoke(new System.Action(() =>
+                {
+                    indicatorPS5.IsBusy = false;
+                    newBitmap = FromBitmapToBitmapImage(tempBM);
+                    if (newBitmap != null)
+                    {
+                        ps5Image.Source = newBitmap;
+                    }
+                }));
+
+            });
         }
+
+        private Bitmap originalBitmapPS5;
+        private bool grayScaleValuePS5 = false;
 
         private void getBackToOriginalPS5_Click(object sender, RoutedEventArgs e)
         {
-
+            BitmapImage bitmapImage = FromBitmapToBitmapImage(originalBitmapPS5);
+            ps5Image.Source = bitmapImage;
+            grayScaleValuePS5 = false;
         }
 
         private void uploadFilePS5_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPEG Image|*.jpg;*.jpeg";
 
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    zoomScale = 1;
+                    ScaleTransform scale = new ScaleTransform(zoomScale, zoomScale);
+                    Image.LayoutTransform = scale;
+                    string extension = Path.GetExtension(openFileDialog.FileName);
+
+                    Bitmap bitmap = new Bitmap(openFileDialog.FileName);
+                    originalBitmapPS5 = bitmap;
+                    BitmapImage bitmapImage = FromBitmapToBitmapImage(bitmap);
+                    ps5Image.Source = bitmapImage;
+
+                }
+                catch
+                {
+                    MessageBoxResult result = MessageBox.Show("Podczas próby odczytu pliku coś poszło nie tak.");
+                }
+            }
         }
 
         private void zoomSliderPS5_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            double zoom = e.NewValue;
+            ScaleTransform scale = new ScaleTransform(zoom, zoom);
+            ps5Image.LayoutTransform = scale;
+        }
 
+        private void grayScalePS5_Click(object sender, RoutedEventArgs e)
+        {
+            if (ps5Image.Source == null)
+            {
+                return;
+            }
+            indicatorPS5.IsBusy = true;
+            Bitmap bitmap = ImageSourceToBitmap(ps5Image.Source);
+            BitmapImage newBitmap = null;
+            Bitmap tempBM = bitmap;
+            var task = Task.Run(() =>
+            {
+                for (int i = 0; i < tempBM.Width; i++)
+                {
+                    for (int j = 0; j < tempBM.Height; j++)
+                    {
+                        System.Drawing.Color color = tempBM.GetPixel(i, j);
+                        byte grayColor = (byte)(0.21 * color.R + 0.71 * color.G + 0.071 * color.B);
+                        tempBM.SetPixel(i, j, System.Drawing.Color.FromArgb(grayColor, grayColor, grayColor));
+                    }
+                }
+            });
+
+            grayScaleValuePS5 = true;
+            task.ContinueWith((t) =>
+            {
+                Application.Current.Dispatcher.Invoke(new System.Action(() =>
+                {
+                    indicatorPS5.IsBusy = false;
+                    newBitmap = FromBitmapToBitmapImage(tempBM);
+                    if (newBitmap != null)
+                    {
+                        ps5Image.Source = newBitmap;
+                    }
+                }));
+
+            });
+        }
+
+        private void binUserValue_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+-");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
-    #endregion
+    
 }
